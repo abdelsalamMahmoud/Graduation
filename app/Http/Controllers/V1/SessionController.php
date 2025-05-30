2ad5f4d616317f40874d273f41241b86a0909eb9
abdelsalamMahmoud <abdelslammahmoud684@gmail.com>
2025-05-31 02:39:21 +0300

    update timezone and today's sessions

--- app/Http/Controllers/V1/SessionController.php
+++ app/Http/Controllers/V1/SessionController.php
@@ -59,10 +59,17 @@ class SessionController extends Controller
     public function teacher_today_sessions()
     {
         try {
-            $todaySessions = Session::where('teacher_id',auth('api')->user()->id)->where('date', Carbon::today()->format('Y-m-d'))->with('student:id,fullName')->paginate(10);
-            return $this->apiResponse($todaySessions,"these are today's sessions",200);
+            $todaySessions = Session::where('teacher_id', auth('api')->user()->id)
+                ->where('date', Carbon::today()->format('Y-m-d'))
+                ->with([
+                    'student:id,fullName',
+                    'teacher.teacherinfo:user_id,link'
+                ])
+                ->paginate(10);
+
+            return $this->apiResponse($todaySessions, "these are today's sessions", 200);
         } catch (\Exception $exception) {
-            return $this->apiResponse(null,'please try again',404);
+            return $this->apiResponse(null, 'please try again', 404);
         }
     }
 
